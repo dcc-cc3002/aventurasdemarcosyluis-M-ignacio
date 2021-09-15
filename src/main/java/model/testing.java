5cf0897281e8ac1e7e5b044e45cbb255d680ec09
Ignacio <70610318+M-ignacio@users.noreply.github.com>
2021-09-15 16:57:58 -0300

    I'm testing the commit

--- src/main/java/model/testing.java
+++ src/main/java/model/testing.java
@@ -1,6 +1,6 @@
 package model;
 
-public class prueba {
+public class testing {
     public static void main(String[] args) {
         Hero h = new Hero(0,10,0,0,"h");
         System.out.println(h.effectiveHit(50));
